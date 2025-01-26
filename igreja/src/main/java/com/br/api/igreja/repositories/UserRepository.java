f5872ff1e1c33219c370ef6d36fe3c472e389f07
Lucas Paz <lucaspaz283@gmail.com>
2025-01-26 15:01:32 -0300

    database connection

---
+++ igreja/src/main/java/com/br/api/igreja/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.br.api.igreja.repositories;
+
+import com.br.api.igreja.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
