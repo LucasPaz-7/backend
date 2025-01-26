b7e98c0de616e7ba1bb2005376b7fbcf1ea7f511
Lucas Paz <lucaspaz283@gmail.com>
2025-01-26 15:25:56 -0300

    AuthenticationService

---
+++ igreja/src/main/java/com/br/api/igreja/service/AuthenticationService.java
@@ -0,0 +1,33 @@
+package com.br.api.igreja.service;
+
+
+import com.br.api.igreja.entities.User;
+import com.br.api.igreja.repositories.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService, AuthenticationManager authenticationManager) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+    }
+
+    public String login(String email, String password) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+        return jwtService.generateToken(user.getEmail());
+    }
+}
+
